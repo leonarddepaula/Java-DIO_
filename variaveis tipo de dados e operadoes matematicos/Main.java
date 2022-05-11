d6cadf9b2e19f6bec05a1fb68a9f2f32c04fa4dc
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2022-05-11 00:55:36 -0300

    :toda: bootcamp  de Java GFT

---
+++ variaveis tipo de dados e operadoes matematicos/Main.java
@@ -0,0 +1,35 @@
+ public class Main {
+     public static void main(String[] args){
+
+        int i ;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+
+       /** j = 15; impossivel pois j é uma constante*/ 
+
+       int quantidadeProduto = 50;
+       final int NUMERO_TENTATIVAS = 5;
+
+       System.out.println(i);
+       System.out.println(I);
+       System.out.println(j);
+       System.out.println(quantidadeProduto);
+       System.out.println(NUMERO_TENTATIVAS);
+
+
+
+
+
+
+     }
+ }
