f690de5a3130804d787f58104016eef782f56564
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2022-05-12 15:29:00 -0300

    atulizando

--- EntendendoMetodosJava/exe02/Main.java
+++ EntendendoMetodosJava/exe02/Main.java
@@ -1,15 +1,50 @@
 package exe02;
 
-public class Main {
 
-    public static void main (String[] args){
+public class Main {
+    
+    public static void main(String[] args){
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
+
+        System.out.println("b1 || b2 " + (b1 || b2));
+        System.out.println("b1 || b3 " + (b1 || b3));
+
+        System.out.println("b1 ^ b2 " + (b1 ^ b2));
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
         
-        //quadrilatero
-        Quadrilatero.area(3);
-        Quadrilatero.area(5, 5);
-        Quadrilatero.area(7, 8, 9);
-        Quadrilatero.area(5f, 5f);
+        System.out.println("!b1 " + (!b1));
+        System.out.println("!b1 " + (!b1));
+
+        int i1 = 10;
+        int i2 = 5;
+        float f1 = 20f;
+        float f2 = 50f;
+
+        System.out.println("((i1 + i2) < (f2 - f1)) && true " + (((i1 + i2) < (f2 - f1)) && true));
+        System.out.println("((i1 > i2) < (f2 < f1)) " + ((i1 + i2) < (f2 - f1)));
+
+        double salarioMensal = 1234.342d;
+        double mediaSalario = 10500d;
+
+        int quantidadeDependentes = 4;
+        int mediaDependentes = 2;
+
+        System.out.println((salarioMensal < mediaDependentes) && (quantidadeDependentes >= mediaDependentes));
+
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes =  quantidadeDependentes >= mediaDependentes;
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+        System.out.println("recebAxilio: " + recebeAuxilio);
 
     }
-    
 }
