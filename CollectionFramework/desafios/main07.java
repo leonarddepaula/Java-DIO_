413a7b806f21fd4c8433bd2d9d058d820049413a
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2022-05-16 19:05:04 -0300

    Os desafios estão meio fora de ordem mas eucomentei oque cada um faz. atenção pra o desafio do vinho implementei 3 soluções para ele porem la na plataforma da DiO_. só uma esta funcionando

---
+++ CollectionFramework/desafios/main07.java
@@ -0,0 +1,23 @@
+//vinoooo
+
+import java.util.Scanner;
+
+public class main07 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String T = scan.nextLine();
+        String respostasCompetidores = scan.nextLine();
+        String[] respostasCompetidoresSplit = respostasCompetidores.split(" ");
+
+        int count = 0;
+
+        for (int i = 0 ; i < respostasCompetidoresSplit.length ; i++) {
+            if (respostasCompetidoresSplit[i].equals(T)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
