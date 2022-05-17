9cc352cb761984dc6eb0b54f2caa8c60812a9aad
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2022-05-16 23:19:53 -0300

    Projeto cadastro de funcionario + salario + bonificação  com Orientação Objetos. Utilizando herança, polimorfismo e encapsulamento

---
+++ CollectionFramework/Abstraindo Situações do Mundo Real Com Programação Orientada a Objetos/GFT-Start-5/models/OperadorDeCaixa.java
@@ -0,0 +1,21 @@
+package models;
+public class OperadorDeCaixa extends FuncionarioCLT {
+
+   
+
+   
+    public OperadorDeCaixa(String nome, String documento, double valorSalario, Endereco endereco) {
+        super(nome, documento, valorSalario, endereco);
+    }
+
+    @Override
+    public String toString() {
+        return "OperadorDeCaixa{" +
+        "nome='" + nome +'\'' +
+        ", documento='" + documento + '\'' +
+        ", valorSalario=" + valorSalario +
+        ", endereco=" + endereco.getBairro() +
+            "}";
+    }
+    
+}
